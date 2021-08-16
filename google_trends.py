bb44729786e7a132ba12ab527bbda7c762a387dc
DylanRowe4 <dylan.rowe04@gmail.com>
2021-08-16 18:11:10 -0500

    Change code for Manchester United

--- google_trends.py
+++ google_trends.py
@@ -3,10 +3,10 @@ from pytrends.request import TrendReq
 import datetime
 
 #initiate instance for trends in England
-pytrend = TrendReq(hl='GB-ENG', tz=360)
+pytrend = TrendReq(hl='en-US', tz=360)
 
-#use Southampton FC as the google trends keywords
-kw_list = ['Southampton Football Club']
+#use Manchester United FC as the google trends keywords
+kw_list = ['Manchester United']
 
 #get dates for time range
 #Specify date ranges here
@@ -20,16 +20,18 @@ categories = {'Sports News': 1077,
               'Soccer':294}
 
 #get information for all categories
-pytrend.build_payload(kw_list=kw_list, timeframe=f"{year} {today}", geo='GB-ENG', cat=0)
+pytrend.build_payload(kw_list=kw_list, timeframe=f"{year} {today}", geo='US-IL', cat=0)
 all_df = pytrend.interest_over_time()
-all_df = all_df.drop('isPartial', axis = 1).rename(columns={'Southampton Football Club':'All'})
+all_df = all_df.drop('isPartial', axis = 1).rename(columns={'Manchester United':'All'})
 
 #loop through remaining categories
 for key in categories:
     try:
         pytrend.build_payload(kw_list=kw_list, timeframe=f"{year} {today}", geo='US-IL', cat=categories[key])
         data = pytrend.interest_over_time()
-        data = data.drop('isPartial', axis = 1).rename(columns={'Southampton Football Club':key})
+        data = data.drop('isPartial', axis = 1).rename(columns={'Manchester United':key})
         all_df = pd.concat([all_df, data], axis = 1)
     except:
         continue
+
+print(all_df)
